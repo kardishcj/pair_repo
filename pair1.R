676fca7d34ced1cb9ed6c4da43018ebad8fc18c8
kardishcj <chris.kardish@gmail.com>
2016-09-30 14:21:33 +0100

    Made a change
    Not much to say

---
+++ pair1.R
@@ -0,0 +1,6 @@
+#rm(list = ls()) this command clears your environment, if you need that
+#setwd('C:/Users/Chris/Documents/Github/pair_repo')
+data(beaver1) 
+data(beaver2)
+
+#practice practice practice
